a66cf5ffa6d24492060c39cdabaa5f024e62f6d2
Auston Robertson <auston.robertson@gmail.com>
2023-07-20 11:04:44 -0600

    finished

--- keeper.client/src/components/KeepCard.vue
+++ keeper.client/src/components/KeepCard.vue
@@ -4,7 +4,7 @@
     class="keep-card fill elevation-3" 
     @click="openModal(keep.id)">
       
-      <div class="img-container fill-x">
+      <div class="img-container fill">
         <img :src="keep.img" :alt="keep.name">
       </div>
   
@@ -67,34 +67,20 @@ import { AppState } from '../AppState'
 <style lang="scss" scoped>
 .keep-card-container{
   max-height: 450px;
+  position: relative;
 }
 .keep-card{
   background-color: var(--cs-1);
   position: relative;
   overflow: hidden;
   border-radius: 0.5rem;
-  transition: all var(--ease);
   cursor: pointer;
-    &:hover{ 
-      .info-container{
-        padding: 0.5rem 0.5rem 1rem 0.5rem;
-      }
-      .img-container img{
-        // width: calc(100% + 1rem);
-        // margin-left: -0.5rem;
-      }
+    &:hover .info-container{
+      padding: 0.5rem 0.5rem 0.75rem 0.5rem;
     }
 }
-.img-container{
-  position: relative;
-  overflow: hidden;
-    img{
-      transition: all var(--ease);
-      width: 100%;
-      height: max-content;
-      margin-left: 0;
-      margin-top: 0;
-    }
+.img-container img{
+  width: 100%;
 }
 .info-container{
   position: absolute;
